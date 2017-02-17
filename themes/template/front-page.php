634ff92c5303cd507e6cff7fef8503b4800292e6
Stephen Hinett <me@stephenhinett.co.uk>
2017-02-17 21:29:31 +0000

    Removed hard coded form in replace for getting the page content

--- themes/template/front-page.php
+++ themes/template/front-page.php
@@ -11,17 +11,15 @@ get_header(); ?>
             <img src="https://placehold.it/550x550" alt="" />
         </div>
         <div class="col-md-6 user-input">
-            <div class="input-wrap">
-                <label for="user_location">Your Location: </label>
-                <input type="text" name="user_location" />
-            </div>
-            <div class="input-wrap">
-                <label for="other_location">Other Location: </label>
-                <input type="text" name="other_location" />
-            </div>
-            <div class="submit-wrap">
-                <button type="submit" class="btn btn-submit">Calculate Distance</button>
-            </div>
+            <?php if ( have_posts() ): ?>
+
+                <?php while ( have_posts() ): the_post(); ?>
+
+                    <?php the_content(); ?>
+
+                <?php endwhile; ?>
+
+            <?php endif; ?>
         </div>
     </div>
 
